2ebe1ab38db58d289237740c1cee941447be8fbd
Ramachandra Junior <ajaykumarch33@yahoo.com>
2018-01-17 06:13:26 +0530

    Add FPS character C++ class

---
+++ BuildingEscape/Source/BuildingEscape/Public/FPSCharacter.h
@@ -0,0 +1,31 @@
+// Copyright Ramachandra Junior 2018
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "FPSCharacter.generated.h"
+
+UCLASS()
+class BUILDINGESCAPE_API AFPSCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AFPSCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	
+	
+};
